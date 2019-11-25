1ffea70f563b00ddaa5aa90979538b0d4fbd6542
Gleb Lysov <lysovg9@gmail.com>
2019-11-25 04:14:21 +0300

    Поправил структуру данных! думать над анимацией

--- src/routes/Shedule.js
+++ src/routes/Shedule.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import './shedule.css';
 
 function ShedulePage(props){
 	const [data,setData] = useState(props.data);
-	const [year, setYear] = useState("");
-	const [orient, setOrient] = useState("");
+	const [course, setCourse] = useState("");
+	const [faculty, setFaculty] = useState("");
 	const [group, setGroup] = useState("");
 	//возможно поменять
 	const [disabled,setDisabled] = useState(true);
-	const [toDisplay,setToDisplay] = useState(props.data.groups);
+	const [toDisplay,setToDisplay] = useState(props.data);
 
 	function isValidInput(){
-		if (!data.year.includes(year)) if (year!=="") return false;
-		if (!data.orient.includes(orient)) if (orient!=="") return false;
+		if (!data.courses.includes(course)) if (course!=="") return false;
+		if (!data.facultys.includes(faculty)) if (faculty!=="") return false;
 		return true;
 	}
 
@@ -21,45 +22,45 @@ function ShedulePage(props){
 	
 				<label>Курс:</label>
 				<input 
-					className="year-input"
-					list="year-select"
-					value={year} 
-					onChange={(e)=>setYear(e.target.value)} 
+					className="course-input"
+					list="course-select"
+					value={course} 
+					onChange={(e)=>setCourse(e.target.value)} 
 		 		/>
-		 		<datalist id="year-select" >
-		 			{data.year.map((e,key)=><option key={key} value={e} />)}		 		
+		 		<datalist id="course-select" >
+		 			{data.courses.map((e,key)=><option key={key} value={e} />)}		 		
 		 		</datalist>
 	
 				<label>Факультет:</label>
 				<input
-					className="orient-input" 
-					list="orient-select"
-					value={orient} 
-					onChange={(e)=>setOrient(e.target.value)} 
+					className="faculty-input" 
+					list="faculty-select"
+					value={faculty} 
+					onChange={(e)=>setFaculty(e.target.value)} 
 				 />
-		 		<datalist id="orient-select" >
-		 			{data.orient.map((e,key)=><option key={key} value={e} />)}	
+		 		<datalist id="faculty-select" >
+		 			{data.facultys.map((e,key)=><option key={key} value={e} />)}	
 		 		</datalist>
 
 		 		{/*Скорее всего перенести это на запрос к серверу, долго фильтровать*/}
 		 		<button onClick={()=>{
 		 			if (!isValidInput()) return;
-		 			const filterYear=(year===""?data.groups:data.groups.filter((e)=>e.year===year));
-		 			const filterOrient=(orient===""?filterYear:filterYear.map((e)=>{
-		 					if (!e.orients.some((sube)=>sube.name===orient)) return;
-		 					return ({
-		 						year:e.year,
-		 						orients:e.orients.filter((sube)=>sube.name===orient)
-		 					});
-		 				}));
+		 			const filterCourse=(course===""?data.courses:data.courses.filter((e)=>e===course));
+		 			const filterFaculty=(faculty===""?data.facultys:data.facultys.filter((e)=>e===faculty));
+
 		 			// const filterGroup=(group===""?filterOrient:filterOrient.map((e)=>{
-		 			// 		if (!e.orients.some((sube)=>sube.groups.includes(group))) return;
+		 			// 		if (!e.facultys.some((sube)=>sube.groups.includes(group))) return;
 		 			// 		return ({
-		 			// 			year:e.year,
-		 			// 			orients:e.orients.map((sube)=>)
+		 			// 			course:e.course,
+		 			// 			facultys:e.facultys.map((sube)=>)
 		 			// 		});
 		 			// 	}));
-		 			setToDisplay(filterOrient);
+
+		 			const newToDisplay={
+		 				courses:filterCourse,
+		 				facultys:filterFaculty
+		 			}
+		 			setToDisplay(newToDisplay);
 		 		}}>Find</button>
 				
 				<div>
@@ -72,78 +73,70 @@ function ShedulePage(props){
 				  />
 				</div>
 
-				<Table data={toDisplay} />
-	
 			</div>
+			<Table data={toDisplay} />
 		</div>
 	);
 }
 
-//Переделать получение информации!!
-let gotData=false;
-
 function Shedule(){
-	const [data,setData] = useState({group:[],orient:[],year:[]});
+	const [data,setData] = useState({available:false,body:{facultys:[],courses:[]}});
 	return(<div>
-		{gotData?<ShedulePage data={data} />:<button onClick={()=>{
-			gotData=true;
-			setData({
-				year:["1","2","3","4","5","6"],
-				orient:["N1","N2","N3","N4","N5","N6","N7"],
-				groups:[{
-					year:"1",
-					orients:[{
-						name:"N1",
-						groups:["111","112","113"]
-					},{
-						name:"N2",
-						groups:["211","212","313"]
-					},{
-						name:"N3",
-						groups:["311","312","313"]
-					}]
-				},{
-					year:"2",
-					orients:[{
-						name:"N1",
-						groups:["121","122","123"]
-					},{
-						name:"N2",
-						groups:["221","222","323"]
-					},{
-						name:"N3",
-						groups:["321","322","323"]
-					}]
-				},{
-					year:"3",
-					orients:[{
-						name:"N1",
-						groups:["131","132","133"]
-					},{
-						name:"N2",
-						groups:["231","232","333"]
-					},{
-						name:"N3",
-						groups:["331","332","333"]
-					}]
-				}]
-			});
+		{data.available?<ShedulePage data={data.body} />:<button onClick={()=>{
+			const newData={
+				available:true,
+				body:{
+					courses:["1","2","3","4","5","6"],
+					facultys:["Н1","Н2","Н3","Н4","Н5","Н6","Н7"]
+				}
+			}
+			setData(newData);
 		}}>Get this data</button>}
 	</div>);
 }
 
 function Table(props){
-	const groups = props.data;
+	const [data,setData] = useState(props.data);
+	const [toPop,setToPop] = useState(props.data.courses.map(()=>false));
+	const [groups,setGroups] = useState([]);
 
+	useEffect(()=>{
+		setData(props.data);
+		setToPop(props.data.courses.map(()=>false));
+	},[props.data]);
+    
+	function loadGroups(cours,fac){
+    	new Promise((ok,err)=>{ok(["БN1-1-1-19","МN1-1-1-19"])}).then((e)=>setGroups(e));
+
+	}
+	/*Все менять! слишком громоздко*/
 	return(<div>
-		{groups.map((e,key)=><div key={key}>
-			<h1>{e.year}</h1>
-			{e.orients.map((sube1,subkey1)=><div key={""+key+subkey1}>
-				<h2>{sube1.name}:</h2>
-				<h3>{sube1.groups.reduce((accum,curr)=>accum+","+curr)}</h3>
-				</div>				
-			)}
-		</div>)}
+		{
+			data.courses.map((course,num)=><div key={num}>
+					<h3 
+						onClick={()=>{
+							let time=0;
+							if (toPop[num]) {
+								const node = document.getElementById("drop-"+num);
+								if (node === null) return;
+								node.className="drop-up";
+								time=600;
+							}
+							setTimeout(function() {setToPop(toPop.map((e_,num_)=>(num===num_)?!e_:e_))}, time);;
+						}} 
+					> {course}-й курс </h3>
+					{(toPop[num])?<div id={"drop-"+num} className="drop-down">
+						{data.facultys.map((fuc,num2)=>
+							<div key={""+num+num2} >
+							<h4 
+								onClick={()=>loadGroups(course,fuc)} 
+							> Факультет {fuc} </h4>
+							{groups.map((e,num3)=><h5 key={""+num+num2+num3}>{e}</h5>)}
+							</div>
+						)}
+							</div>:null}
+				</div>)
+		}
 	</div>);
 }
 
